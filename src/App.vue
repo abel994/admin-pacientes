780f0703961eff1348bf0e0ffd5228fbf117786a
abel994 <abel994@hotmail.com>
2024-02-26 17:08:48 -0600

    73. Primeros pasos con Tailwind y el Proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+  import Header from './components/Header.vue';
 </script>
 
 <template>
-<h1 class="text-6xl bg-">Hola Mundo</h1>
+  <div class="container mx-auto mt-20">
+    <Header />
+  </div>
 </template>
 
 <style scoped>
